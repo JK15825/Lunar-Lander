516677e3d5abd53aa6a855e52bb106db7ecb4008
JK15825 <baseballranger@gmail.com>
2016-08-27 16:09:51 -0500

    this too

--- src/Terrain.java
+++ src/Terrain.java
@@ -11,25 +11,8 @@ public class Terrain
         {
             int x = 0;
             int y = 0;
-            
-            int columnCounter = 0;
+
             Scanner in = new Scanner(new File("map_eventaully.dat"));
-            int type = 0;
-            /*while((type = in.nextInt()) != null)
-            {
-                if(type != 0)
-                {
-                    if(columnCounter = 60)
-                    {
-                        y+=16;
-                        
-                    }
-                    landscape.add(new Point(0,0,type));
-                    x+=16;
-                    columnCounter++;
-                }
-            }*/
-            
         }
         catch(Exception e)
         {
