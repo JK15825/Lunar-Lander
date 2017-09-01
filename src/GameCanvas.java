0beaf6a207f1dd4dffd5234a1a5ba0c48a5c4c7a
DESKTOP-028ERIB\Jett <spmaamil>
2017-09-01 12:13:23 -0500

    TITS

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,4 +1,5 @@
 import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.awt.GLCanvas;
@@ -18,11 +19,11 @@ public class GameCanvas extends GLCanvas implements GLEventListener
 	{
 		GL2 gl = drawable.getGL().getGL2();
 		glu = new GLU();
-		gl.glClearColor(0f,0f,0f,0f);
-		gl.glClearDepth(10f);
-		gl.glEnable(GL2.GL_DEPTH_TEST);
-		gl.glDepthFunc(GL2.GL_LEQUAL);
-		gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST);
+		gl.glClearColor(1.0f,1.0f,1.0f,1.0f);
+		gl.glClearDepth(1.0f);
+		gl.glEnable(GL.GL_DEPTH_TEST);
+		gl.glDepthFunc(GL.GL_LEQUAL);
+		gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
 		gl.glShadeModel(GL2.GL_SMOOTH);
 		
 	}
@@ -37,13 +38,13 @@ public class GameCanvas extends GLCanvas implements GLEventListener
 	private void render(GLAutoDrawable drawable)
 	{
 		GL2 gl = drawable.getGL().getGL2();
-		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
 		gl.glLoadIdentity();
 		
 		float sin = (float)Math.sin(angle);
 	    float cos = (float)Math.cos(angle);
-	    gl.glBegin(GL2.GL_TRIANGLES);
-        gl.glColor3f(1.0f, 0.0f, 0.0f);   // Red
+	    gl.glBegin(GL.GL_TRIANGLES);
+        gl.glColor3f(1.0f, 0.0f, .0f);   // Red
         gl.glVertex2d(-cos, -cos);
         gl.glColor3f(0.0f, 1.0f, 0.0f);   // Green
         gl.glVertex2d(0.0f, cos);
